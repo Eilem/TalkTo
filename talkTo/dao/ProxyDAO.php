9a97453d4dfd47542823020e87230493383f413c
Eilem <eilem.rei@gmail.com>
2012-08-17 20:05:09 -0300

    ProxyDao

---
+++ talkTo/dao/ProxyDAO.php
@@ -0,0 +1,65 @@
+<?php
+
+class ProxyDAO {
+    
+    /**
+    * Cria o Dialogo enviando o objeto Dialogo 
+    * @return id do Dialogo 
+    */
+   public function persistirDialogo(Dialogo $oDialogo) {
+       try{
+           $dao = new DAO();
+           return $dao->persistirDialogo($oDialogo); 
+        }catch(Exception $erro){
+            echo($erro->getMessage());
+        }
+   }
+   
+   /**
+    * Inseri a Mensagem enviando o objeto Mensagem
+    * @param Mensagem $mensagem
+    * @return booleano 
+    */
+   public function persistirMensagem(Mensagem $mensagem) {
+       try{
+          $dao = new DAO();
+          return $dao->persistirMensagem($mensagem);
+        }catch(Exception $erro){
+            echo($erro->getMessage());
+        }
+   }
+   
+    /**
+    * Coleções Mensagens de um determinado Dialogo que foi passado pelo parametro $id
+    * @param int $id
+    * @return array de Mensagens
+    */
+   public function colecaoMensagens($id) {
+       try{
+           $dao = new DAO();
+           return $dao->colecaoMensagens($id);
+        }catch(Exception $erro){
+            echo($erro->getMessage());
+        }
+   }
+   
+    public function obterDialogo($id){
+         try{
+           $dao = new DAO();
+           return $dao->obterDialogo($id);
+        }catch(Exception $erro){
+            echo($erro->getMessage());
+        }
+    }
+    
+    public function validarUsuario(){
+        try{
+            
+        }catch(Exception $erro){
+            echo($erro->getMessage());
+        }
+        
+    }
+    
+   
+}
